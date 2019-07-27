bd48195b782ff9514986e1c460bd41f4071f6805
Daniel Moniz <daniel.moniz@gmail.com>
2019-07-27 13:51:08 -0400

    Remove some unnecessary imports from BronzeBot

--- bronze_bot/bot.py
+++ bronze_bot/bot.py
@@ -1,8 +1,5 @@
 import math
 
-from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
-from rlbot.utils.structures.game_data_struct import GameTickPacket
-
 from util.orientation import Orientation
 from util.vec import Vec3
 from util.debug import draw_debug
