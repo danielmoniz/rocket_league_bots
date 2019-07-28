9c13621c05fd901aa66800a0cf2530438bc1656a
Daniel Moniz <daniel.moniz@gmail.com>
2019-07-27 21:28:11 -0400

    Print line along opposite goal

--- bronze_bot/util/debug.py
+++ bronze_bot/util/debug.py
@@ -1,3 +1,4 @@
+from util.vec import Vec3
 
 def draw_debug(player, renderer, car, target, action_display):
     renderer.begin_rendering()
@@ -5,4 +6,10 @@ def draw_debug(player, renderer, car, target, action_display):
     renderer.draw_line_3d(car.physics.location, target, renderer.white())
     # print the action that the bot is taking
     renderer.draw_string_3d(car.physics.location, 2, 2, action_display, renderer.white())
+
+    # experiment: draw lines along goal
+    renderer.draw_line_3d(player.opposing_goal.location, Vec3(player.opposing_goal.location) + Vec3(800, 0, 0), renderer.white())
+    renderer.draw_line_3d(player.opposing_goal.location, Vec3(player.opposing_goal.location) + Vec3(-800, 0, 0), renderer.white())
+
+
     renderer.end_rendering()
