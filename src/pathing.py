a4915ebe6af2d5adfa97f521e57cf5b273a15a7c
Daniel Moniz <daniel.moniz@gmail.com>
2019-07-28 14:07:24 -0400

    Fix pathing curves by transposing coordinates

--- src/pathing.py
+++ src/pathing.py
@@ -5,12 +5,12 @@ from src.util.vec import Vec3
 
 def compute_shooting_curve(player):
     car_location = Vec3(player.car.physics.location)
-    car_direction = Vec3(player.car.physics.velocity)
+    car_direction = player.game_info['car_orientation'].forward
     ball_location = player.game_info['ball_location']
     ball_to_goal = (Vec3(player.opposing_goal.location) - ball_location).normalized()
+
     coordinates = get_shooting_vectors(
         car_location, car_direction, ball_location, ball_to_goal)
-
     return compute_curve(coordinates)
 
 
@@ -25,7 +25,8 @@ def get_shooting_vectors(car_location, car_direction, ball_location, ball_to_goa
 
 
 def compute_curve(vectors, degree=10):
-    massaged_vectors = get_fortran_array(vectors)
+    transposed = list(zip(*vectors))
+    massaged_vectors = get_fortran_array(transposed)
     return bezier.Curve(massaged_vectors, degree=degree)
 
 
@@ -33,3 +34,11 @@ def get_fortran_array(vectors):
     def vector_to_tuple(vector):
         return tuple(map(lambda x: float(x), vector))
     return np.asfortranarray([vector_to_tuple(x) for x in vectors])
+
+
+def convert_coordinate_to_vector(coord):
+    return Vec3(
+        coord[0][0],
+        coord[1][0],
+        max(coord[2][0], 25),
+    )
