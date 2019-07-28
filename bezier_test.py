ef14deec3f7a0054666b6aa8c34bb82d95ce763b
Daniel Moniz <daniel.moniz@gmail.com>
2019-07-28 08:38:49 -0400

    Add bezier curve test with images

---
+++ bezier_test.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+import bezier
+import matplotlib
+from matplotlib import pyplot as plt
+
+
+# Shooting example
+# car at 0.0, facing left (-1, 0)
+# ball at (2, 2)
+# hit ball toward net - right (1, 0)
+# coordinates description:
+    # 1. car position
+    # 2. position in front of car
+    # 3. position in front of ball and net
+    # 4. position just in front of ball and net (closer)
+    # 5. ball position
+nodes = np.asfortranarray([
+    # [0.0, -0.3, 1.0, 2.0, 1.0, 1.5],
+    # [0.0, -0.3, -1.0, 2.0, 1.0, 1.5],
+    [0.0, -1.0, 1, 1.6, 2.0],
+    [0.0, 0.0, 2.0, 2.0, 2.0],
+])
+
+curve = bezier.Curve(nodes, degree=1)
+print(curve)
+print(curve.__dict__.keys())
+curve.plot(256)
+plt.show()
