d944a10c9131ed28340a6be6fa46ef58629e1daa
Daniel Moniz <daniel.moniz@gmail.com>
2019-08-03 11:34:48 -0400

    Compute needed info for deciding to shoot

--- src/strategy/shoot_with_power.py
+++ src/strategy/shoot_with_power.py
@@ -2,7 +2,7 @@ import math
 
 from src import pathing
 from src.util.vec import Vec3
-from src.util.angle import find_correction
+from src.util import angle
 
 def enact(player):
     distance_from_ball = player.game_info['distance_from_ball']
@@ -10,12 +10,23 @@ def enact(player):
     very_short_range = 400
     short_range = 1000
 
+    car_direction = player.game_info['car_orientation'].forward
+    ball_to_goal = player.game_info['ball_to_goal']
+
     curve = pathing.compute_shooting_curve(
         player.game_info['car_location'],
-        player.game_info['car_orientation'].forward,
+        car_direction,
         player.game_info['ball_location'],
-        player.game_info['ball_to_goal'],
+        ball_to_goal,
     )
+
+    car_to_ball = (player.game_info['car_location'] - player.game_info['ball_location'])
+    position_angle_threshold = math.radians(15)
+    facing_angle_threshold = math.radians(15)
+
+    at_very_short_range = distance_from_ball < short_range
+    in_line_position = angle.find_correction(car_to_ball, ball_to_goal) < position_angle_threshold
+    in_line_direction = angle.find_correction(car_direction, car_to_ball) < facing_angle_threshold
 
     if distance_from_ball < very_short_range:# and in a decent line
         # @TODO Dodge into a specific part of the ball
