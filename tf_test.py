85cd022fa8cf85a73b6bfa6432e85b11b6f48ee1
Daniel Moniz <daniel.moniz@gmail.com>
2019-12-08 13:08:29 -0500

    Get Tensorflow sample file working

--- tf_test.py
+++ tf_test.py
@@ -4,27 +4,28 @@ import tensorflow as tf
 model = tf.keras.models.load_model('frankenbot/model.h5')
 
 player_data = {
-    'pos_x': 300,
-    'pos_y': -2000,
-    'pos_z': 200,
+    'pos_x': 0.322,
+    'pos_y': 0.313,
+    'pos_z': 0.0084,
 }
 
 input_data = np.array([
     player_data['pos_x'],
     player_data['pos_y'],
     player_data['pos_z'],
-    0,  # @TODO boost quantity
+    0.70,  # @TODO boost quantity
     3000, # self.game_info.ball_location.x,
     -4500, # self.game_info.ball_location.y,
     500, # self.game_info.ball_location.z,
     100,  # @TODO velocity x
     100,  # @TODO velocity y
     5,  # @TODO velocity z
-    1,  # @TODO map
-    1,  # @TODO team - top priority!
+    1.0,  # @TODO map
+    1.0,  # @TODO team - top priority!
 ])
-input_data = np.int64(input_data)
+input_data = np.float64(input_data)
 print(input_data.shape[0])
+print(input_data)
 # model_input = np.ndarray((1, 3), buffer=np.array(np.int64([1, 2, 3])))
 model_input = np.ndarray((1, input_data.shape[0]), buffer=input_data)
 print(model_input.shape)
