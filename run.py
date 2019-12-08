2f21ce91889bdfa3d25ca1cc6b1e797796a862f8
Daniel Moniz <daniel.moniz@gmail.com>
2019-12-08 12:08:49 -0500

    Update requirements and ignored files to run ML algorithm

--- run.py
+++ run.py
@@ -10,23 +10,23 @@ DEFAULT_LOGGER = 'rlbot'
 
 if __name__ == '__main__':
 
-    try:
-        from rlbot.utils import public_utils, logging_utils
+    # try:
+    #     from rlbot.utils import public_utils, logging_utils
 
-        logger = logging_utils.get_logger(DEFAULT_LOGGER)
-        if not public_utils.have_internet():
-            logger.log(logging_utils.logging_level,
-                       'Skipping upgrade check for now since it looks like you have no internet')
-        elif public_utils.is_safe_to_upgrade():
-            pipmain(['install', '-r', 'requirements.txt', '--upgrade', '--upgrade-strategy=eager'])
+    #     logger = logging_utils.get_logger(DEFAULT_LOGGER)
+    #     if not public_utils.have_internet():
+    #         logger.log(logging_utils.logging_level,
+    #                    'Skipping upgrade check for now since it looks like you have no internet')
+    #     elif public_utils.is_safe_to_upgrade():
+    #         pipmain(['install', '-r', 'requirements.txt', '--upgrade', '--upgrade-strategy=eager'])
 
-            # https://stackoverflow.com/a/44401013
-            rlbots = [module for module in sys.modules if module.startswith('rlbot')]
-            for rlbot_module in rlbots:
-                sys.modules.pop(rlbot_module)
+    #         # https://stackoverflow.com/a/44401013
+    #         rlbots = [module for module in sys.modules if module.startswith('rlbot')]
+    #         for rlbot_module in rlbots:
+    #             sys.modules.pop(rlbot_module)
 
-    except ImportError:
-        pipmain(['install', '-r', 'requirements.txt', '--upgrade', '--upgrade-strategy=eager'])
+    # except ImportError:
+    #     pipmain(['install', '-r', 'requirements.txt', '--upgrade', '--upgrade-strategy=eager'])
 
     try:
         if len(sys.argv) > 1 and sys.argv[1] == 'gui':
